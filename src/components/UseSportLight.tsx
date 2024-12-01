8214c51f5e80fbac131357cc21bf9e5ad4e8bb61
Danish Fareed <104269116+danish-fareed@users.noreply.github.com>
2024-12-01 20:14:42 +0500

    Update UseSportLight.tsx

--- src/components/UseSportLight.tsx
+++ src/components/UseSportLight.tsx
@@ -2,17 +2,22 @@
 
 import { useTheme } from "next-themes";
 import SpotlightCursor from "./SpotLightCursorExample";
+import { useEffect } from "react";
 
  
 const UseSpotLight = () => {
+    var mounted = false;
     const {theme} = useTheme();
+    useEffect(() => {
+        mounted = true;
+    }) 
     const spotlightConfig = {
       radius: 700,
-      color: theme === 'dark' ? '19, 41, 97': '114, 187, 255',
-      brightness: theme === 'dark' ? 1 : 1,
-      overlayColor: theme === 'dark' ? '0, 0, 0' : '242, 242, 247',
-      overlay: theme === 'dark' ? 0.2 : 0.5,
-      outerColor: theme === 'dark' ? '19, 41, 97' :'255, 255, 255',
+      color: mounted && theme === 'dark' ? '19, 41, 97': '114, 187, 255',
+      brightness: mounted && theme === 'dark' ? 1 : 1,
+      overlayColor: mounted && theme === 'dark' ? '0, 0, 0' : '242, 242, 247',
+      overlay: mounted && theme === 'dark' ? 0.2 : 0.5,
+      outerColor: mounted && theme === 'dark' ? '19, 41, 97' :'255, 255, 255',
       outerBrightness: 0,
       smoothing: 0.1,
     };
